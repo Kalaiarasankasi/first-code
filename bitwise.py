0d29ec9ea62716ff159bab2125752cb2cbd1f424
Kalaiarasankasi <kalaikasi166@gmail.com>
2024-01-31 20:03:46 +0530

    bitwise

---
+++ bitwise.py
@@ -0,0 +1,19 @@
+# Bitwise operators
+"""
+&  AND
+|  OR
+^  XOR
+~  NOT
+<< zero fill left shift
+>> signed right shift
+"""
+a = 25
+b = 45
+print(a&b)
+print(a|b)
+print(a^b)
+print(~a)
+print(a<<2)
+print(a>>2)
+
+
