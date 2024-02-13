bc306729849748773f17b22c80d767c8cd521206
Kalaiarasankasi <kalaikasi166@gmail.com>
2024-02-13 20:26:12 +0530

    First commit

---
+++ Arthmetic.py
@@ -0,0 +1,23 @@
+# Arthmetic operators
+
+"""
++  Addition
+-  subraction
+*  multipication
+/  division
+%  module
+** exponentiation
+// floor division
+"""
+
+a = 123
+b = 10
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a / b)
+print(a ** b)
+print(2**3)
+
